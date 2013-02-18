ce2815d522b1cfc679c14618281a4ff9e14b7b71
Tony Okusanya <Anthony.Okusanya@gmail.com>
2013-02-18 10:41:11 -0600

    iteration e

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -14,6 +18,19 @@ class Product < ActiveRecord::Base
 
 
   def shout
-    "I am shouting from the products controller"
+    'I am shouting from the products controller'
   end
+
+  private
+
+# ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Thedepot::Application.routes.draw do
+  resources :line_items
+
+
+  resources :carts
+
+
   get "store/index"
 
   resources :products
